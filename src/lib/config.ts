bc12b24e3a57e45363fd20c1c3740781a3d5f122
andre-brandao <brandaoandre00@gmail.com>
2023-10-03 11:55:01 -0300

    header, footer and base styles added

---
+++ src/lib/config.ts
@@ -0,0 +1,6 @@
+import { dev } from "$app/environment";
+
+export const title = 'Portifolio André Brandão';
+export const description = 'My App Description';
+export const url = dev ? 'http://localhost:5173/' : 'https://myapp.com';    
+
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -2,4 +2,11 @@
 	import Contribuitions from '$lib/Git/Contribuitions.svelte';
 </script>
 
-<Contribuitions />
+<!-- <Contribuitions /> -->
+
+<p>
+	Lorem ipsum dolor sit amet consectetur adipisicing elit. Illo veniam recusandae earum maiores,
+	eveniet magni nisi iure, consequuntur sit temporibus vel dignissimos, quia ab quis distinctio
+	soluta. Consequatur eum deleniti voluptas ea, suscipit sapiente earum, nam assumenda incidunt iure
+	quis quia asperiores est animi molestiae ducimus. Excepturi ipsa libero expedita?
+</p>
