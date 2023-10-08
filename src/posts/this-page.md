6e8c7192bcdc4f474f7795394c33cdc2c98175a0
andre-brandao <brandaoandre00@gmail.com>
2023-10-08 11:14:37 -0300

    post and image

---
+++ src/posts/this-page.md
@@ -0,0 +1,21 @@
+---
+title: This Page
+description: How was this page made.
+date: '2023-4-14'
+categories:
+  - SvelteKit
+  - Markdown
+published: true
+---
+
+<script>
+import  SelectTheme from '$lib/components/select_theme.svelte';
+</script>
+
+<SelectTheme/>
+
+## Table of Contents
+
+## Description
+
+This page was made using [SvelteKit](https://kit.svelte.dev/), [Svelte](https://svelte.dev/), [Markdown](https://www.markdownguide.org/), and [Github Pages](https://pages.github.com/).
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -15,18 +15,22 @@
 		{#each data.posts as post}
 			<a href={post.slug}>
 				<li class="post">
-					<p class="title">{post.title}<p/>
+					<p class="title">{post.title}</p>
+					<p />
 					<p class="date">{formatDate(post.date)}</p>
 					<p class="description">{post.description}</p>
 				</li>
 			</a>
 		{/each}
 		<li class="coming-soon">
-			<p class="title">Theres More<p/>
+			<p class="title">Theres More</p>
+			<p />
 			<!-- <p class="date">{formatDate(post.date)}</p> -->
-			<p class="description">I have other projects, but I did not write a post for it, most are in C or Java for college classes.</p>
+			<p class="description">
+				I have other projects, but I did not write a post for it, most are in C or Java for college
+				classes.
+			</p>
 		</li>
-		
 	</ul>
 </section>
 
@@ -47,7 +51,7 @@
 		grid-template-columns: repeat(auto-fill, minmax(25rem, 1fr));
 		gap: 2rem;
 	}
-	
+
 	.post {
 		/* max-inline-size: var(--size-content-2); */
 		background-color: var(--surface-4);
@@ -57,14 +61,11 @@
 
 		/* give it a min height */
 		min-block-size: 400px;
-
 	}
 	/* when hover post it gets bigger*/
 	.post:hover {
 		background-color: var(--surface-1);
 		transition: background-color 0.2s ease-in-out;
-
-
 	}
 
 	.post:not(:last-child) {
