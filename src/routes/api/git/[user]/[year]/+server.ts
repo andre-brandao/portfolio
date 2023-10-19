289f8d3a6259be8e13c8472ac456c0f583384f7d
André de Oliveira Brandão <brandaoandre00@gmail.com>
2023-10-19 11:48:42 -0300

    api update

--- src/routes/api/git/[user]/[year]/+server.ts
+++ src/routes/api/git/[user]/[year]/+server.ts
@@ -3,76 +3,102 @@ import { parseHTML } from 'linkedom';
 import type { RouteParams } from './$types.js';
 
 export async function GET({ params, setHeaders }) {
-	const year = 60 * 60 * 24 * 365;
+   //    const year = 60 * 60 * 24 * 365;
 
-	// https://vercel.com/docs/edge-network/caching#cdn-cache-control
-	setHeaders({
-		'Access-Control-Allow-Origin': '*',
-		'Cache-Control': `public, s-maxage=${year}`,
-		'CDN-Cache-Control': `public, s-maxage=${year}`,
-		'Vercel-CDN-Cache-Control': `public, s-maxage=${year}`
-	});
+   // https://vercel.com/docs/edge-network/caching#cdn-cache-control
+   //    setHeaders({
+   //       'Access-Control-Allow-Origin': '*',
+   //       'Cache-Control': `public, s-maxage=${year}`,
+   //       'CDN-Cache-Control': `public, s-maxage=${year}`,
+   //       'Vercel-CDN-Cache-Control': `public, s-maxage=${year}`,
+   //    });
 
-	const html = await getContributions(params);
-	return json(parseContributions(html));
+   setHeaders({
+      'Access-Control-Allow-Origin': '*', // allow CORS
+      'Cache-Control': `public, s-maxage=${60 * 60 * 24 * 365}`, // one year
+   });
+
+   //    console.log('getContributions');
+
+   const html = await getContributions(params);
+
+   //    console.log(html);
+
+   return json(parseContributions(html));
 }
 
 async function getContributions({ user, year }: RouteParams) {
-	let api = `https://github.com/users/${user}/contributions?from=${year}-12-01&to=${year}-12-31`;
+   let api = `https://github.com/users/${user}/contributions?from=${year}-12-01&to=${year}-12-31`;
 
-	const isCurrentYear = new Date().getFullYear().toString() === year;
+   const isCurrentYear = new Date().getFullYear().toString() === year;
 
-	if (isCurrentYear) {
-		const date = new Date().toLocaleDateString('en-CA');
-		const month = date.split('-')[1];
-		api = `https://github.com/users/${user}/contributions?from=${year}-${month}-01&to=${date}`;
-	}
+   if (isCurrentYear) {
+      const date = new Date().toLocaleDateString('en-CA');
+      const month = date.split('-')[1];
+      api = `https://github.com/users/${user}/contributions?from=${year}-${month}-01&to=${date}`;
+   }
 
-	try {
-		const response = await fetch(api);
+   try {
+      const response = await fetch(api);
 
-		if (!response.ok) {
-			throw new Error(`Failed to fetch: ${response.status}`);
-		}
+      if (!response.ok) {
+         throw new Error(`Failed to fetch: ${response.status}`);
+      }
 
-		return await response.text();
-	} catch (e) {
-		throw new Error(`Something went wrong: ${e}`);
-	}
+      return await response.text();
+   } catch (e) {
+      throw new Error(`Something went wrong: ${e}`);
+   }
 }
 
 function parseContributions(html: string) {
-	const { document } = parseHTML(html);
+   const { document } = parseHTML(html);
 
-	const rows = document.querySelectorAll<HTMLTableRowElement>('tbody > tr');
+   const rows = document.querySelectorAll<HTMLTableRowElement>('tbody > tr');
 
-	const contributions = [];
+   const contributions = [];
 
-	for (const row of rows) {
-		const days = row.querySelectorAll<HTMLTableCellElement>('td:not(.ContributionCalendar-label)');
+   for (const row of rows) {
+      const days = row.querySelectorAll<HTMLTableCellElement>(
+         'td:not(.ContributionCalendar-label)'
+      );
+      //   console.log('Days:');
 
-		const currentRow = [];
+      //   console.log(days);
 
-		for (const day of days) {
-			const data = day.innerText.split(' ');
+      const currentRow = [];
 
-			if (data.length > 1) {
-				const contribution = {
-					count: data[0] === 'No' ? 0 : +data[0],
-					name: data[3].replace(',', ''),
-					month: data[4],
-					day: +data[5].replace(',', ''),
-					year: +data[6],
-					level: +day.dataset.level!
-				};
-				currentRow.push(contribution);
-			} else {
-				currentRow.push(null);
-			}
-		}
+      const dayOfWeekElement = row.querySelector('td.ContributionCalendar-label');
+      //   if (dayOfWeekElement) {
+      const dayOfWeek = dayOfWeekElement.querySelector('span:first-child').textContent;
+      //  currentRow.push(dayOfWeek);
+      //   }
 
-		contributions.push(currentRow);
-	}
+      for (const day of days) {
+         const data = day.innerText.split(' ');
+         console.log('data:');
+         console.log(data);
 
-	return contributions;
+         //  const date = day.getAttribute('data-date');
+
+         if (data.length > 1) {
+            const contribution = {
+               count: data[0] === 'No' ? 0 : +data[0],
+               date: day.dataset.date,
+               name: dayOfWeek,
+               month: data[3],
+               day: data[4],
+               year: day.dataset.date?.split('-')[0],
+               level: +day.dataset.level!,
+            };
+            currentRow.push(contribution);
+         } else {
+            currentRow.push(null);
+         }
+      }
+
+      contributions.push(currentRow);
+   }
+
+   return contributions;
 }
